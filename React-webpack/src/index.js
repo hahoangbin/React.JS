d737df1c73d1080e225ea73c6687c2f62fc4b5b7
Ha Ngoc Duong <hahoangbin999@gmail.com>
2023-08-23 22:36:50 +0700

    Create project ReactJS + webpack

---
+++ React-webpack/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// Create component App
+function App() {
+    return (
+        <div>
+            <h1>Hello my friend!</h1>
+            <h2>Ok roi day ae oi!</h2>
+        </div>
+    )
+}
+
+// Render component App vao #root element
+ReactDOM.render(<App />, document.getElementById('root'))
