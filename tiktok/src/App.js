9f4233ec48e43093803a7423e1f387c66dc1b8dc
Ha Ngoc Duong <hahoangbin999@gmail.com>
2023-08-26 22:02:28 +0700

    useState trong react hook

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { useState } from 'react'
+ 
 function App() {
+  const orders = [111, 222, 333]
+  const [counter, setCounter] = useState(() => {
+    const total = orders.reduce((total, cur) => total + cur)
+    return total
+  })
+
+  const handleIncrease = () => {
+    setCounter(counter => counter + 1)
+    setCounter(counter => counter + 1)
+    setCounter(counter => counter + 2)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{counter}</h1>
+        <button onClick={handleIncrease}>Increase</button>
       </header>
     </div>
   );
