e124b7a9fc56a1c758963bbf517fec79e016ead0
Ha Ngoc Duong <hahoangbin999@gmail.com>
2023-08-27 16:14:34 +0700

    todolist with useState

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -3,87 +3,36 @@ import './App.css';
 import { useState } from 'react'
  
 function App() {
-  const orders = [111, 222, 333]
-  const [counter, setCounter] = useState(() => {
-    const total = orders.reduce((total, cur) => total + cur)
-    return total
+  const [job, setJob] = useState('')
+  const [jobs, setJobs] = useState(() => {
+    const storageJobs = JSON.parse(localStorage.getItem('jobs'))
+    return storageJobs ?? []
   })
-  const [gift, setGift] = useState('Chua co phan thuong')
-  const [name, setName] = useState('')
-  const [checked, setChecked] = useState([])
 
-  const gifts = [
-    'CPU i9',
-    'RAM 32GB RGB',
-    'RGB Keyboard',
-  ]
-  const courses = [
-    {
-      id: 1,
-      name: "HTML, CSS",
-    },
-    {
-      id: 2,
-      name: "ReactJS",
-    },
-    {
-      id: 3,
-      name: "Javascript",
-    },
-  ]
-
-  const handleIncrease = () => {
-    setCounter(counter => counter + 1)
-    setCounter(counter => counter + 1)
-    setCounter(counter => counter + 2)
-  }
-  const handleGetReward = () => {
-    var index = Math.floor(Math.random() * gifts.length)
-    setGift(gifts[index]);
-  }
-  const handleCheck = (id) => {
-    setChecked(prev => {
-      const isChecked = checked.includes(id)
-      if (isChecked) {
-        // unCheck
-        return checked.filter(item => item !== id)
-      } else {
-        return [...prev, id]
-      }
+  const handleSubmit = () => {
+    setJobs(prev => {
+      const newJobs = [...prev, job]
+      const jsonJobs = JSON.stringify(newJobs)
+      localStorage.setItem('jobs', jsonJobs)
+      return newJobs
     })
+    setJob('')
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>{counter}</h1>
-        <h2>{gift}</h2>
-        <button onClick={handleIncrease}>Increase</button>
-        <br />
-        <button onClick={handleGetReward}>Lấy thưởng</button>
-        <br />
-        <div style={{display: 'flex'}}>
-          <input value={name} onChange={e => setName(e.target.value)}></input>
-          <button onClick={e => setName('Nguyen Van BBB')}>set name</button>
-        </div>
-        {courses.map(course => (
-          <div key={course.id}>
-            <input 
-              type='radio'
-              checked={checked === course.id}
-              onChange={() => setChecked(course.id)} />
-              {course.name}
-            <input 
-              style={{marginLeft: '50px'}}
-              type='checkbox'
-              checked={checked.includes(course.id)}
-              onChange={() => handleCheck(course.id)} />
-              {course.name}
-          </div>
-        ))}
+         <div>
+          <input 
+            value={job}
+            onChange={e => setJob(e.target.value)} />
+          <button onClick={handleSubmit}>Add</button>
+          <ul>
+            {jobs.map((job, index) => (
+              <li key={index}>{job}</li>
+            ))}
+          </ul>
+         </div>
       </header>
     </div>
   );
